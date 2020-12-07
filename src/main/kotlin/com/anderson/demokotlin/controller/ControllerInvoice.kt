f14668017413ce7fc2e1c05674f4b6b01a93ed96
Vitorassi <vitorassi8@gmail.com>
2020-12-07 12:33:50 -0300

    - rename ControllerIncoice.kt

--- src/main/kotlin/com/anderson/demokotlin/controller/ControllerInvoice.kt
+++ src/main/kotlin/com/anderson/demokotlin/controller/ControllerInvoice.kt
@@ -7,41 +7,34 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/invoice")
-class ControllerIncoice {
-
-    @Autowired
-    private val serviceInvoice: ServiceInvoice? = null
+class ControllerInvoice(var serviceInvoice:ServiceInvoice) {
 
     @GetMapping
     fun getList(): List<Invoice> {
-
-        val allInvoice = serviceInvoice!!.getAllInvoice("teste")
-
+        val allInvoice = serviceInvoice.getAllInvoice("teste")
         return allInvoice;
     }
 
 
     @PostMapping
-    fun pos(@RequestBody invoice: Invoice): Invoice {
-
+    fun post(@RequestBody invoice: Invoice): Invoice {
         val ret = serviceInvoice!!.addInvoice(invoice);
-
         return ret;
     }
 
     @DeleteMapping("/{id}")
     fun delete( @PathVariable id:Int ){
-        serviceInvoice!!.removeInvoice(id)
+        serviceInvoice.removeInvoice(id)
     }
 
     @GetMapping("/{id}")
     fun get( @PathVariable id:Int ): Invoice?{
-        return serviceInvoice!!.getInvoice(id)
+        return serviceInvoice.getInvoice(id)
     }
 
     @PutMapping("/{id}")
     fun put( @RequestBody invoice: Invoice){
-        serviceInvoice!!.putInvoice(invoice)
+        serviceInvoice.putInvoice(invoice)
     }
 
 }
